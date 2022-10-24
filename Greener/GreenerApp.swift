b09f2f8b29a0b66dadc5d05618744ff2afcea94c
raphpay <41484808+raphpay@users.noreply.github.com>
2022-10-24 10:23:13 +0200

    Initial commit

---
+++ Greener/GreenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenerApp.swift
+//  Greener
+//
+//  Created by Raphaël Payet on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct GreenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
